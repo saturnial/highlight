8009f5ce2251422364b7bf762b206ded03ce82c5
Jason Mylen <jmylen@Jasons-MacBook-Air.local>
2013-04-12 19:11:59 -0700

    fixing index handler

--- src/highlite/highlighter/views.py
+++ src/highlite/highlighter/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render_to_response
 from django.shortcuts import RequestContext
 
 def index(request):
-  facebook_profile = request.user.get_profile().get_facebook_profile() 
-  return render_to_response('index.html', {'facebook_profile': facebook_profile}) 
-
+  return render_to_response('index.html', {}) 
 
 def feed(request):
+  facebook_profile = request.user.get_profile().get_facebook_profile() 
   return render_to_response('feed.html', {})
 
 
