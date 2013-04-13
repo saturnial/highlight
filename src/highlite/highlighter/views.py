ee45f9bab6b328aadd913127eaa4be4b2bee61f9
Kayvon Tehranian <ktehranian@gmail.com>
2013-04-13 12:58:12 -0700

    Refactored create to be a class-based view with CSRF protection

--- src/highlite/highlighter/views.py
+++ src/highlite/highlighter/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from django.views.generic.base import View
+from django.template import RequestContext
 
 import forms
 import foursquare
@@ -12,20 +14,27 @@ def feed(request):
   facebook_profile = request.user.get_profile().get_facebook_profile()
   return render_to_response('feed.html', {'facebook_profile': facebook_profile})
 
-def create(request):
-  if request.method == 'POST':
+class CreateHighlight(View):
+  """Class-based view for handling the create highlight process."""
+
+  def get(self, request):
+    form = forms.CreateHighlight()
+    return render_to_response('create.html',
+                              {'form': form},
+                              context_instance=RequestContext(request))
+
+  def post(self, request):
     form = forms.CreateHighlight(request.POST)
     if form.is_valid():
-      return HttpResponseRedirect('/')
-  else:
-    form = forms.CreateHighlight()
+      new_highlight = form.save()
+      return HttpResponseRedirect('/lite/create')
+    else:
+      return render_to_response('create.html', {'form': form})
+
     #fsq_auth_token = request.session.get('fsq_access_token')
     #logging.info(fsq_auth_token)
     #fsq_client = foursquare.Foursquare(access_token=fsq_auth_token)
 
     #coffee_places = fsq_client.venues.search(params={'query': 'coffee',
     #                                                 'near': 'San Francisco, CA'})
-  return render_to_response('create.html', {
-      'form': form,
       #'coffee_search': coffee_places
-  })
