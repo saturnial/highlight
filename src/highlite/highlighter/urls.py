cd13369eb6fe7a98519b02fd1768c2089be97e77
Jason Mylen <jmylen@Jasons-MacBook-Air.local>
2013-04-13 00:12:27 -0700

    Adding facebook icon

--- src/highlite/highlighter/urls.py
+++ src/highlite/highlighter/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
+    url(r'feed', views.feed, name='feed'),
     url(r'', views.index, name='index'),
-    url(r'feed/', views.feed, name='feed'),
 )
 
 
