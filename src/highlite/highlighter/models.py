a317407e69e88ce6177c46d0c2a3217a98f642e3
Kayvon Tehranian <ktehranian@gmail.com>
2013-04-12 19:31:09 -0700

    Added unicode representation of Entry

--- src/highlite/highlighter/models.py
+++ src/highlite/highlighter/models.py
@@ -3,8 +3,11 @@ from django.db import models
 class Entry(models.Model):
   user = models.CharField(max_length=50)
   day = models.DateField(auto_now_add=True)
- 
+
+  def __unicode__(self):
+    return u"%s's response on %s" % (self.user, self.day)
+
 class Highlight(models.Model):
-  entry = models.ForeignKey(Entry) 
+  entry = models.ForeignKey(Entry)
   text = models.TextField()
   score = models.IntegerField()
