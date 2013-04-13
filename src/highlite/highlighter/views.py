054164aebe09a15138364605b326a476427d452d
Kayvon Tehranian <ktehranian@gmail.com>
2013-04-13 11:44:51 -0700

    Create now extends base and view passes form

--- src/highlite/highlighter/views.py
+++ src/highlite/highlighter/views.py
@@ -16,16 +16,16 @@ def create(request):
   if request.method == 'POST':
     form = forms.CreateHighlight(request.POST)
     if form.is_valid():
-      return HttpResponseRedirect('/thanks/')
+      return HttpResponseRedirect('/')
   else:
     form = forms.CreateHighlight()
     #fsq_auth_token = request.session.get('fsq_access_token')
     #logging.info(fsq_auth_token)
-    #fsq_client = foursquare.Foursquare(access_token=fsq_auth_token) 
-      
+    #fsq_client = foursquare.Foursquare(access_token=fsq_auth_token)
+
     #coffee_places = fsq_client.venues.search(params={'query': 'coffee',
     #                                                 'near': 'San Francisco, CA'})
-  return render_to_response(request, 'create.html', {
+  return render_to_response('create.html', {
       'form': form,
       #'coffee_search': coffee_places
   })
